3b2ed6cb63f142ca60e9b410d0c69701675424a6
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-14 18:57:44 +0200

    another one

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask,render_template
+
+posts=[{ "author":"Joe" ,"title" :"Blog post 1","content":"This is my first blog post"},{"author":"minnies","title":"Mouse","content":"I swear i am the real minnie mouse !"}]
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "<h1> Homepage!</h1>"
+    return render_template("home.html",post=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
+
