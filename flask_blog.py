9d575a4a0f98e6dac7a9afc5dd4dce8a07511016
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-14 20:12:30 +0200

    last commit for today

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
-posts=[{ "author":"Joe" ,"title" :"Blog post 1","content":"This is my first blog post"},{"author":"minnies","title":"Mouse","content":"I swear i am the real minnie mouse !"}]
+
+posts=[{ "author":"Joe" ,"title" :"Blog post 1","content":"This is my first blog post","date_posted":"12/3/2020"},{"author":"minnies","title":"Mouse","content":"I swear i am the real minnie mouse !","date_posted":"15/6/2020"}]
 
 app = Flask(__name__)
 
@@ -11,7 +12,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html",title="About")
 
 if __name__=="__main__":
     app.run(debug=True)
