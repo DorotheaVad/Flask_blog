8543c8d4b74c1c899d609c06eb796c438196c336
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-19 21:34:36 +0200

    initialized register.html and login.html for the homonymous forms

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,url_for
-
+from forms import RegistrationForm,LoginForm
 
 posts=[{ "author":"Joe" ,"title" :"Blog post 1","content":"This is my first blog post","date_posted":"12/3/2020"},{"author":"minnies","title":"Mouse","content":"I swear i am the real minnie mouse !","date_posted":"15/6/2020"}]
 
@@ -15,6 +15,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html",title="About")
+
+@app.route("/register")
+def register():
+    form=RegistrationForm()
+    return render_template("register.html",title="Register",form=form)
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template("login.html",title="Login",form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
