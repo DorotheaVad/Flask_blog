d414bdb4311c725e607c8942d7de311fe56f431d
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-20 13:56:53 +0200

    Some other changes

--- forms.py
+++ forms.py
@@ -13,7 +13,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
    
     email=StringField("Email",validators=[DataRequired(),Email()])
-    password=StringField("Password",validators=[DataRequired()])
+    password=PasswordField("Password",validators=[DataRequired()])
     remember=BooleanField("Remember Me")
     submit=SubmitField("Login")
     
