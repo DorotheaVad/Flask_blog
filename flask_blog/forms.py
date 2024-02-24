211064d42dc2880b0b601b4ff527f2c44a381635
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-24 02:47:08 +0200

    Added logout and account views for authenticated users. Also started using login_manager and its properties...

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,BooleanField,PasswordField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
-
+from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
+from flask_blog.models import User
 class RegistrationForm(FlaskForm):
 
     username= StringField("Username",validators=[DataRequired(),Length(min=2,max=20)])
@@ -10,6 +10,16 @@ class RegistrationForm(FlaskForm):
     confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),EqualTo("password")])
     submit=SubmitField("Sign Up")
 
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken. Please choose another username !")
+
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is already signed. Please use another email !")
 
 class LoginForm(FlaskForm):
    
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template,url_for,flash,redirect
+from flask import render_template,url_for,flash,redirect,request
 from flask_blog.forms import RegistrationForm,LoginForm
-from flask_blog import app
+from flask_blog import app,db,bcrypt
+from flask_blog.models import User,Post
+from flask_login import login_user,current_user,logout_user,login_required
 
 posts=[{ "author":"Joe" ,"title" :"Blog post 1","content":"This is my first blog post","date_posted":"12/3/2020"},{"author":"minnies","title":"Mouse","content":"I swear i am the real minnie mouse !","date_posted":"15/6/2020"}]
 
@@ -16,19 +18,42 @@ def about():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form=RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account created for {form.username.data}!",category="success")
-        return redirect(url_for("home"))
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user=User(username=form.username.data,email=form.email.data,password=hashed_password)                                                                           
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created. Now you are able to login",category="success")
+        return redirect(url_for("login"))
     return render_template("register.html",title="Register",form=form)
 
 @app.route("/login",methods=["GET","POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form=LoginForm()
     if form.validate_on_submit():
-        if form.email.data=="admin@blog.com" and form.password.data=="password":
-            flash(f'Succesful login !',category="success")
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            flash("You loged in succesfully! We are redirecting you to your homepage...",category="success")
+            next_page=request.args.get('next')
+            if next_page:
+                return redirect(next_page)
             return redirect(url_for("home"))
         else:
             flash("Login Unsuccesful. Please check password and email !",category="danger")
     return render_template("login.html",title="Login",form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+    
+@app.route("/my_account")
+@login_required
+def account():
+    return render_template("account.html",title="My Account")
