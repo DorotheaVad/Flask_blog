65073aaae21b7588f2bfc43fab77ca66e34a640f
Dorothea Vadraska <vadraskad@gmail.com>
2024-02-24 20:03:00 +0200

    Account view edits

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template,url_for,flash,redirect,request
-from flask_blog.forms import RegistrationForm,LoginForm
+from flask_blog.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from flask_blog import app,db,bcrypt
 from flask_blog.models import User,Post
 from flask_login import login_user,current_user,logout_user,login_required
@@ -56,5 +56,6 @@ def logout():
 @app.route("/my_account")
 @login_required
 def account():
+    form=UpdateAccountForm()
     image_file=url_for("static",filename="profile_pics/"+current_user.image_file)
-    return render_template("account.html",title="My Account",image_file=image_file)
+    return render_template("account.html",title="My Account",image_file=image_file,form=form)
